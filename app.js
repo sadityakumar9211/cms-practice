3d7a446823ee3b52645778b07bed0a55189cb44a
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-03-22 23:31:20 +0530

    FIX: tracking empty git dirs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
+const hbs = require('express-handlebars')
 
 /*configure mongoose*/
 mongoose
@@ -18,9 +19,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+/*
+Setup views engine to use handlebars
+*/
+app.engine('handlebars', hbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 /* Routes */
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'))
